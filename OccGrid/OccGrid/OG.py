45fbe477bb79d2ff9b22e39e3f2499e8e1efa257
ankuraga <ankuraga@gmail.com>
2016-09-20 23:15:01 -0700

    BREAKTHROUGH: Using image processing to do prediction in map

--- OccGrid/OccGrid/OG.py
+++ OccGrid/OccGrid/OG.py
@@ -21,10 +21,10 @@ B_z[Observation.HIT][0, 0] = 1;
 B_z[Observation.MISS][1, 1] = 1;
 
 #used for updates in a dynamic world HMM
-B_z[Observation.NO_OBS][0, 0] = 0.5;
-B_z[Observation.NO_OBS][0, 1] = 0.5;
-B_z[Observation.NO_OBS][1, 0] = 0.5;
-B_z[Observation.NO_OBS][1,1] = 0.5;
+B_z[Observation.NO_OBS][0, 0] = 0;
+B_z[Observation.NO_OBS][0, 1] = 1;
+B_z[Observation.NO_OBS][1, 0] = 1;
+B_z[Observation.NO_OBS][1,1] = 0;
 
 #class to hold data for the cell.
 class Cell:
@@ -50,8 +50,10 @@ class Cell:
         self.state_curr = State.OCC
         self.Q_curr[0, 0] = 1  #init all states to occupied
         #Static Environment
-        self.A[0,0] = 1
-        self.A[1,1] = 1
+        self.A[0,0] = 0.6
+        self.A[0,1] = 0.4
+        self.A[1,1] = 0.8
+        self.A[1,0] = 0.2
 
     def set_A(self, A_next):
         print "Setting A"
@@ -71,7 +73,10 @@ class Cell:
     #Perform a belief update on the cell given an Observation()
     def step(self, obs):
         #Qt+1 = Qt * A * B(h)
-        self.Q_next = self.Q_curr.dot( self.A.dot( B_z[obs] ))
+        #p1 = self.Q_curr.dot( self.A)
+        #p2 = p1.dot(B_z[obs] )
+        self.Q_next = self.Q_curr.dot( self.A ).dot( B_z[obs] )
+        #self.Q_next = p2.copy();
         norm = self.Q_next[0, 0] + self.Q_next[0, 1];
         self.Q_next /= max(norm, epsilon);       
         self.Q_curr = self.Q_next.copy()
@@ -84,7 +89,7 @@ class Cell:
         print self.Q_curr;    
     
     def getState(self):
-        if (self.Q_curr[0, 0] >= 0.5):
+        if (self.Q_curr[0, 0] > 0.5):
             return State.OCC
         return State.EMPTY
 
