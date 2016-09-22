87ad3af1668aaa7ca0e945a7714668b2892b772d
ankuraga <ankuraga@gmail.com>
2016-09-22 03:01:16 -0700

    friggin lost files

--- OccGrid/estimateState.py
+++ OccGrid/estimateState.py
@@ -1,5 +1,5 @@
 import os
-import sys, qetopt
+import sys, getopt
 import time
 from updateBelief import *
 from updateBelief import Cell
@@ -11,111 +11,112 @@ from dataHandler import *
 from utility import *
 
 OBS_MAP = {0: Observation.NO_OBS,
-127: Observation MISS,
+127: Observation.MISS,
 255: Observation.HIT}
 
-STATE_MAP = {State.EMPTY: O,
+STATE_MAP = {State.EMPTY: 0,
 State.OCC: 255}
 
 def usage():
-	print "usage"
-	print "\t -h —help : prints this usage information"
-	print "\t -f —filename : REQUIRED. Path to H5PY file. If file exists in same location, only name is sufficient"
-	print "\t -i —index : REQUIRED. Index of frame to be estimated. Should be >= 20"
-	print "\t -d —debug: OPTIONAL. Flag to save estimated state map for each time step. default = False"
-	return;
-	
+    print "usage"
+    print "\t -h —help : prints this usage information"
+    print "\t -f —filename : REQUIRED. Path to H5PY file. If file exists in same location, only name is sufficient"
+    print "\t -i —index : REQUIRED. Index of frame to be estimated. Should be >= 20"
+    print "\t -d —debug: OPTIONAL. Flag to save estimated state map for each time step. default = False"
+    return;
+    
 #function to generate state map from state probability
 def getStateMap(state_map, state_prob):
-	state_map[state_prob > 0.5] = 255
-	state_map[state_prob <= 0.5] = 0
-return state_map
+    state_map[state_prob > 0.5] = 255
+    state_map[state_prob <= 0.5] = 0
+    return state_map
 
 def estimateState(fileName, idx, debug):
-	HEIGHT = 100
-	WIDTH = 100
-	T_STEPS = 5
-	State_Map = np.zeros(HEIGHT, WIDTH), dtype = np.uint8);
-	State_Prob = np.full(HEIGHT, WIDTH), 1); #All cells are assumed empty initially
-	State_Map = getStateMap(State_Map, State_Prob)
-	saveImagePNG(State_Map, "init.png");
-	Cell_Grid = [[Cell() for w in range(WIDTH)] for h in range(HEIGHT)]
-	
-	#read and save ground truth form database
-	
-	#Gnd_Truth = getData(fileName, ‘ground_truth’, idx);
-	#savelmagePNG(Gnd_Truth[:, :, 0], ‘GT_' + str(idx) + ‘.png’);
-	Gnd_Truth = getData(fileNamne, ‘ground_truth’, idx - T_STEPS, idx);
-	
-	#read laser scan data form database
-	Laser_Scans = getData(fileName, ‘occupancy’, idx -T_STEPS, idx);
-	
-	for t in range( 0, T_STEPS):
-		for y in range( HEIGHT):
-			for x in range( WIDTh):
-				obs = Laser_Scans [y, x, t]
-				Cell_Grid[y][x].setPrior(State_Prob[y] [x])
-				ifobs >0:
-					Cell_Grid[y][x].updateCellState(OBS_MAP[obs])
-					State_Prob[y][x] = Cell_Grid[y][x].getPrior()
-					
-	State_Map = getStateMap(State_Map, State_Prob)
-	saveImagePNG(State_Prob*255, ‘prob_1'+str(t + idx)+'.png');
-	saveImagePNG(State_Map, ‘temp_est_map'+str(t + idx)+ .png’);
-	State_Map, State_Prob = predictState(State_Map, State_Prob)
-	saveImagePNG(State_ProbwZ5S, ‘prob_2' +str(t + idx) +‘.png’);
-	
-	if debug:
-		saveImagePNG(Gnd_Truth[:, :, t], ‘GT’+str(t + idx)+’.png’);
-		savelmagePNG(State_Map, ‘est_map_’+str(t + idx)+’,png’);
-		compareWithGroundTruth(Gnd_Truth[:, :, t], State_Map, ‘diff'+str(t + idx)+’.png’)
-		savelmagePNG(Laser_scans[:, :, t], 'scan_' + str(t + idx)+ '.png’);
-		
-	#error = compareWithGroundTruth(Gnd_Truth[:, :, t], State_Map)
-	return error
+    HEIGHT = 100
+    WIDTH = 100
+    T_STEPS = 20
+    State_Map = np.zeros((HEIGHT, WIDTH), dtype = np.uint8);
+    State_Prob = np.full((HEIGHT, WIDTH), 1); #All cells are assumed empty initially
+    State_Map = getStateMap(State_Map, State_Prob)
+    saveImagePNG(State_Map, "init.png");
+    Cell_Grid = [[Cell() for w in range(WIDTH)] for h in range(HEIGHT)]
+    
+    #read and save ground truth form database
+    
+    #Gnd_Truth = getData(fileName, 'ground_truth', idx);
+    #savelmagePNG(Gnd_Truth[:, :, 0], 'GT_' + str(idx) + '.png');
+    Gnd_Truth = getData(fileNamne, 'ground_truth', idx - T_STEPS, idx);
+    
+    #read laser scan data form database
+    Laser_Scans = getData(fileName, 'occupancy', idx -T_STEPS, idx);
+    
+    for t in range( 0, T_STEPS):
+        for y in range( HEIGHT):
+            for x in range( WIDTh):
+                obs = Laser_Scans [y, x, t]
+                Cell_Grid[y][x].setPrior(State_Prob[y] [x])
+                if obs >0:
+                    Cell_Grid[y][x].updateCellState(OBS_MAP[obs])
+                    State_Prob[y][x] = Cell_Grid[y][x].getPrior()
+                    
+    State_Map = getStateMap(State_Map, State_Prob)
+    saveImagePNG(State_Prob*255, 'prob_1'+str(t + idx)+'.png');
+    saveImagePNG(State_Map, 'temp_est_map'+str(t + idx)+ '.png');
+    State_Map, State_Prob = predictState(State_Map, State_Prob)
+    saveImagePNG(State_ProbwZ5S, 'prob_2' +str(t + idx) +'.png');
+    
+    if debug:
+        saveImagePNG(Gnd_Truth[:, :, t], 'GT'+str(t + idx)+'.png');
+        savelmagePNG(State_Map, 'est_map_'+str(t + idx)+',png');
+        compareWithGroundTruth(Gnd_Truth[:, :, t], State_Map, 'diff'+str(t + idx)+'.png')
+        savelmagePNG(Laser_scans[:, :, t], 'scan_' + str(t + idx)+ '.png');
+        
+    #error = compareWithGroundTruth(Gnd_Truth[:, :, t], State_Map)
+    return error
 
-#Entry funnction
+#Entry function
 def main(argv):
-	startTime = time.timeO;
-	fileName = None;
-	idx = 0;
-	saveDebugImages = False;
-	
-	try:
-		opts, args = getopt.getopt(argv, ‘hf:i:d’, ["help","filename = ", "index = "]);
-		if not opts:
-		print ‘No optons supplied’
-		usageO;
-		sys.exit();
-		except getopt.GetoptError:
-		usage();
-		sys.exit(2);
-		
-		for opt, arg in opts:
-		if opt in ("-h", "-—help"):
-			usage();
-			sys.exit();
-		
-		for opt, arg in opts:
-			if opt in ("-f", "--filename"):
-				fileName = arg;
-			elif opt in ("-i", "--index"):
-			dx = int(arg);
-			if idx <20:
-				print "The index needs to be >= 20."
-				usage();
-				sys.exit();
-			elif opt == "-d":
-				saveDebugImages = True;
-		
-		if fileName is None or idx = = 0:
-		print "Required arguments missing"
-		usage();
-		sys.exit();
-		
-		estimateState(fileName, idx, saveDebugImages)
-		print "Total time taken %s minutes" % ((time.time() - startTime)/60);
-	
+
+    startTime = time.time();
+    fileName = None;
+    idx = 0;
+    saveDebugImages = False;
+    
+    try:
+        opts, args = getopt.getopt(argv, 'hf:i:d', ["help","filename = ", "index = "]);
+        if not opts:
+            print "No options supplied"
+            usage();
+            sys.exit();
+    except getopt.GetoptError:
+        usage();
+        sys.exit(2);
+        
+        for opt, arg in opts:
+            if opt in ("-h", "-—help"):
+                usage();
+                sys.exit();
+        
+        for opt, arg in opts:
+            if opt in ("-f", "--filename"):
+                fileName = arg;
+            elif opt in ("-i", "--index"):
+                idx = int(arg);
+                if idx <20:
+                    print "The index needs to be >= 20."
+                    usage();
+                    sys.exit();
+            elif opt == "-d":
+                saveDebugImages = True;
+        
+        if fileName is None or idx == 0:
+            print "Required arguments missing"
+            usage();
+            sys.exit();
+        
+        estimateState(fileName, idx, saveDebugImages)
+        print "Total time taken %s minutes" % ((time.time() - startTime)/60);
+    
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
 
--- OccGrid/predictState.py
+++ OccGrid/predictState.py
@@ -6,30 +6,30 @@ OBSTACLE_RADIUS = 6
 #function to draw a circle (obstacle) on state map and state probability
 #State map is unit8, but state prob is float
 def drawObstacle(img, (x, y), r, value):
-cv2.circle(img, (int(x), int(y)), r, (value), -1)
-return img
+    cv2.circle(img, (int(x), int(y)), r, (value), -1)
+    return img
 
 # function to add obstcles to the global state
 # First a state is predicted on basis of the posterior
 # then the posterior is corrected to account for the added obstcle area
 def predictState(state_map, state_prob):
-b, thresh = cv2.threshold(state_map, 127, 255, cv2.ThRESH_BINARY)
-connectivity =4
-
-c_idx, labels, stats, centroids = cv2.connectedComponentsWithstats(thresh, connectivity)
-#for each centroid (not background), create an obstacle
-
-for comp in range(1, c_idx):
-stat = stats[comp]
-centroid = centroids[comp]
-#do this only if area of obstacle is > 4
-if stat[4] >4:
-cx = stat[0] + (stat[2]/2)
-cy = stat[1] + (stat[3]/2)
-cx = centroid[0]
-cy = centroid[1]
-obs_prob = state_prob[centroid[1], centroid [0]]
-
-state_map = drawObstacle(state_map, (cx, cy), OBSTACLE_RADIUS, 255)
-State_prob = drawObstacle(state_prob, (cx, cy), OBSTACLE_RADIUS, obs_prob)
-return (state_map, state_prob)
+    b, thresh = cv2.threshold(state_map, 127, 255, cv2.ThRESH_BINARY)
+    connectivity =4
+    
+    c_idx, labels, stats, centroids = cv2.connectedComponentsWithstats(thresh, connectivity)
+    #for each centroid (not background), create an obstacle
+    
+    for comp in range(1, c_idx):
+        stat = stats[comp]
+        centroid = centroids[comp]
+        #do this only if area of obstacle is > 4
+        if stat[4] >4:
+            cx = stat[0] + (stat[2]/2)
+            cy = stat[1] + (stat[3]/2)
+            cx = centroid[0]
+            cy = centroid[1]
+            obs_prob = state_prob[centroid[1], centroid [0]]
+            
+            state_map = drawObstacle(state_map, (cx, cy), OBSTACLE_RADIUS, 255)
+            state_prob = drawObstacle(state_prob, (cx, cy), OBSTACLE_RADIUS, obs_prob)
+    return (state_map, state_prob)
